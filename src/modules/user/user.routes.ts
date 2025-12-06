d700e3500f9fa28ca583ec7b8c5b352c7616843f
Kawser420 <muhammadkawser202@gmail.com>
2025-12-06 16:59:22 +0600

    feat: all user CRUD operation seccessfully

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -3,6 +3,19 @@ import { userControllers } from "./user.controller";
 
 const router = express.Router();
 
+// post
 router.post("/", userControllers.createUser);
+
+// get users
+router.get("/", userControllers.getUser);
+
+// get single user
+router.get("/:id", userControllers.getSingleUser);
+
+// update user
+router.put("/:id", userControllers.updateUser);
+
+// delete user
+router.delete("/:id", userControllers.deleteUser);
 
 export const userRoutes = router;
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,5 +1,6 @@
 import { pool } from "../../config/db";
 
+// post user service
 const createUser = async (name: string, email: string) => {
   const result = await pool.query(
     `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
@@ -9,6 +10,43 @@ const createUser = async (name: string, email: string) => {
   return result;
 };
 
+// get user service
+const getUser = async () => {
+  const result = await pool.query(`SELECT * FROM users`);
+  return result;
+};
+
+// get single user service
+const getSingleUser = async (id: string) => {
+  const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+
+  return result;
+};
+
+// update user service
+const updateUser = async (name: string, email: string, id: string) => {
+  const result = await pool.query(
+    `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`,
+    [name, email, id]
+  );
+
+  return result;
+};
+
+// delete user service
+const deleteUser = async (id: string) => {
+  const result = await pool.query(
+    `INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`,
+    [id]
+  );
+
+  return result;
+};
+
 export const userServices = {
   createUser,
+  getUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
 };
