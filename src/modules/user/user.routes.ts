bc91a494657964d64bb3c5968c2d02498399b25c
Kawser420 <muhammadkawser202@gmail.com>
2025-12-10 05:44:00 +0600

    user routes finished

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,26 +1,5 @@
 import express, { Request, Response } from "express";
 import { userControllers } from "./user.controller";
-
-const router = express.Router();
-
-// post
-router.post("/", userControllers.createUser);
-
-// get users
-router.get("/", userControllers.getUser);
-
-// get single user
-router.get("/:id", userControllers.getSingleUser);
-
-// update user
-router.put("/:id", userControllers.updateUser);
-
-// delete user
-router.delete("/:id", userControllers.deleteUser);
-
-export const userRoutes = router;
-import express, { Request, Response } from "express";
-import { userControllers } from "./user.controller";
 import logger from "../../middleware/logger";
 import auth from "../../middleware/auth";
 
